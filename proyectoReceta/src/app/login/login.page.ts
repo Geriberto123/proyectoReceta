bc97fb1d95dc5535b1edf79939d830621c59b869
Juan Esteban <jordoneztobon@gmail.com>
2025-01-16 19:57:49 -0500

    Actividad Login Password

--- proyectoReceta/src/app/login/login.page.ts
+++ proyectoReceta/src/app/login/login.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
-  templateUrl:'./login.page.html',
+  templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: false,
 })
@@ -11,29 +12,31 @@ export class LoginPage implements OnInit {
   errorMessage: any;
   formErrors = {
     email: [
-      { type: 'required', message: 'El correo es obligatorio'},
-      { type: 'email', message: 'El correo no es valido'}
+      { type: 'required', message: 'El correo es obligatorio' },
+      { type: 'email', message: 'El correo no es válido' }
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es obligatoria' },
+      { type: 'minlength', message: 'La contraseña debe tener al menos 6 caracteres' }
     ]
-  }
+  };
 
-  constructor(
-    private formBuilder: FormBuilder,
-  ) {
+  constructor(private formBuilder: FormBuilder) {
     this.loginForm = this.formBuilder.group({
       email: new FormControl('', Validators.compose([
-        Validators.required, 
+        Validators.required,
         Validators.email
       ])),
       password: new FormControl('', Validators.compose([
-        Validators.minLength(6), 
-        Validators.required
+        Validators.required,
+        Validators.minLength(6)
       ]))
-    })
-   }
-
-  ngOnInit() {
+    });
   }
-  loginUser(credentials: any){
-    console.log(credentials, "Credenciales de login")
+
+  ngOnInit() {}
+
+  loginUser(credentials: any) {
+    console.log(credentials, "Credenciales de login");
   }
 }
