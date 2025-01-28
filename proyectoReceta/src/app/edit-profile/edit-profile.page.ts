fc92715f601f9da8b3c87722044c518c5821c55b
Juan Esteban <jordoneztobon@gmail.com>
2025-01-28 15:04:55 -0500

    Version final de Editar Perfil

--- proyectoReceta/src/app/edit-profile/edit-profile.page.ts
+++ proyectoReceta/src/app/edit-profile/edit-profile.page.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, AlertController } from '@ionic/angular';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-edit-profile',
@@ -7,16 +8,62 @@ import { ModalController } from '@ionic/angular';
   styleUrls: ['./edit-profile.page.scss'],
   standalone: false
 })
-export class EditProfilePage {
-  @Input() user_data: any;
+export class EditProfilePage implements OnInit {
+  user_data: any = {
+    name: '',
+    last_name: '',
+    email: '',
+    image: '',
+  };
 
-  constructor(private modalController: ModalController) {}
+  constructor(
+    private modalController: ModalController,
+    private alertController: AlertController
+  ) {}
+
+  ngOnInit() {}
 
   dismissModal() {
-    this.modalController.dismiss(); 
+    this.modalController.dismiss();
   }
 
-  saveChanges() {
-    this.modalController.dismiss(this.user_data); 
+  async saveChanges() {
+    this.modalController.dismiss(this.user_data);
+  }
+
+  async changeImage() {
+    const alert = await this.alertController.create({
+      header: 'Selecciona una opción',
+      message: '¿Deseas tomar una foto con la cámara o seleccionar una desde los documentos?',
+      buttons: [
+        {
+          text: 'Cámara',
+          handler: () => {
+            this.capturePhoto(CameraSource.Camera);
+          },
+        },
+        {
+          text: 'Documentos',
+          handler: () => {
+            this.capturePhoto(CameraSource.Photos);
+          },
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+
+  async capturePhoto(source: CameraSource) {
+    try {
+      const capturedPhoto = await Camera.getPhoto({
+        resultType: CameraResultType.DataUrl,
+        source: source,
+        quality: 100,
+      });
+      this.user_data.image = capturedPhoto.dataUrl;
+    } catch (error) {
+      console.error('Error al capturar la foto:', error);
+    }
   }
 }
